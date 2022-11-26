16e7b94787310777aefa3164c3aad23c88afd378
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-11-26 01:47:02 -0500

    Added staff emailing and added more buttons

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 import Employee from './src/employee.js';
 import express from 'express';
+// email with nodemailer
+import nodemailer from 'nodemailer';
+// get email and password from .env file
+import dotenv from 'dotenv';
+dotenv.config();
+
+let user = process.env.EMAIL;
+let password = process.env.PASS;
+
+// set up nodemailer
+const transporter = nodemailer.createTransport({
+    service: 'FastMail',
+    auth: {
+        user: user,
+        pass: password
+    }
+});
 
 const employee = new Employee("./database/jobs_list.db");
 const app = express();
@@ -37,6 +54,37 @@ app.get('/employees/edit', (req, res) => {
     });
 });
 
+app.get('/employees/email', (req, res) => {
+    const { id } = req.query;
+    employee.getEmployeeById(id).then((data) => {
+        res.render('email', {employee: data[0]});
+    });
+});
+
+app.get('/employees/email/send', (req, res) => {
+    // send email with text from req.query.text
+    const { id, subject } = req.query;
+    employee.getEmployeeById(id).then((data) => {
+        let email = data[0].email;
+        let text = req.query.subject
+        let message = req.query.message;
+        const mailOptions = {
+            from: user,
+            to: email,
+            subject: text,
+            text: message
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);    
+            }
+        });
+        res.redirect('/employees');
+    });
+});
+
 app.get('/employees/update', (req, res) => {
     // find what is being updated
     const { id, firstName, lastName, email, department, salary } = req.query;
@@ -55,7 +103,6 @@ app.get('/employees/update', (req, res) => {
     if (salary != null) {
         employee.updateEmployeeById(id, null, null, null, null, salary);
     }
-    console.log();
     res.redirect('/employees');
 });
 
